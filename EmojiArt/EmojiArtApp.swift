84051b331211dfed8f0e2d3c59bed1a0d7368dc2
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-19 21:53:30 -0400

    Update EmojiArtApp.swift

--- EmojiArt/EmojiArtApp.swift
+++ EmojiArt/EmojiArtApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct EmojiArtApp: App {
     let documentVM = EmojiArtDocumentVM()
+    let paletteStore = PaletteStore(named: "Default")
     
     var body: some Scene {
         WindowGroup {
