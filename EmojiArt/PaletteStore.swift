bcdbdc44b48e9f2b776885748ee50200543548c4
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-19 21:33:35 -0400

    add PaletteStore as the view-model

---
+++ EmojiArt/PaletteStore.swift
@@ -0,0 +1,55 @@
+//
+//  PaletteStore.swift
+//  EmojiArt
+//
+//  Created by mixemer on 4/19/22.
+//
+
+import SwiftUI
+
+struct Palette {
+    var name: String
+    var emojis: String
+    var id: Int
+    
+    fileprivate init(name: String, emojis: String, id: Int) {
+        self.name = name
+        self.emojis = emojis
+        self.id = id
+    }
+}
+
+class PaletteStore: ObservableObject {
+    let name: String
+    
+    @Published var palettes = [Palette]()
+    
+    init (named name: String) {
+        self.name = name
+    }
+    
+    // MARK: - Intends
+    
+    func palette(at index: Int) -> Palette {
+        let safeIndex = min(max(index, 0), palettes.count - 1)
+        return palettes[safeIndex]
+    }
+    
+    @discardableResult
+    func removePalette(at index: Int) -> Int {
+        if palettes.count > 1, palettes.indices.contains(index) {
+            palettes.remove(at: index)
+        }
+        return index % palettes.count
+    }
+    
+    func insertPalette(named name: String, emojis: String? = nil, at index: Int = 0) {
+        let unique = (palettes.max(by: { $0.id < $1.id })?.id ?? 0) + 1
+        
+        let palette = Palette(name: name, emojis: emojis ?? "", id: unique)
+        let safeIndex = min(max(index, 0), palettes.count - 1)
+        palettes.insert(palette, at: safeIndex)
+    }
+}
+
+
